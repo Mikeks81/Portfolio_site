5bb5789233befa9c01299fafd51030e533ea3164
Michael <Mikeks81@gmail.com>
2015-12-08 10:14:37 -0500

    added landing page fade in. resized images.

---
+++ contact.js
@@ -0,0 +1,20 @@
+$(document).ready(
+	function(){
+		// $("h1").fadeOut(6000);
+		$("#hello").hide();
+		$("#hello").fadeIn(3000);
+
+		$("#enter").hide();
+		$("#enter").fadeIn(3000);
+
+		
+
+		// $("#enter").hide();
+		// $("#enter").fadeIn(2000);	
+});
+
+
+// function submit(){
+// 	alert("Thank you.")
+// }
+
