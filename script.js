f0a2bdd85819220289bf895bcd25b9a7010b0e39
Michael <Mikeks81@gmail.com>
2015-12-31 22:16:40 -0500

    added a shitload of high res images added basic lightbox without navigation on click of thumbnails

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ $(document).ready(
 
 		// ###### Gallery Page #########
 
+		$(".image").on('click',
+			function(){
+				$("#lb_image").attr('src',($(this).attr('alt')));
+				$("#outer_lightbox").fadeIn(500);
+				$("#inner_lightbox").fadeIn(500);
+				$("#lb_image").fadeIn(500);
+			});
+
 		// $(".image").mouseenter( //on mouseenter of image the image zooms 5px
 		// 	function(){
 		// 		$(this, "img").animate({width: "+=5px",
@@ -20,6 +28,9 @@ $(document).ready(
 		// 		$(this, "img").animate({width: "-=5px",
 		// 			height: "-=5px"});
 		// 	});
+
+		// Used CSS to do the animation instead
+		// DO NOT DELETE ABOVE CODE - IMG SELECTOR CODE IS DOPES!!
 
 		//########### Contact Page ###########		
 });		
