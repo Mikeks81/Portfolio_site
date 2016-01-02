218f9804ce3a201dcdde961587d0ac67e24b9afc
Michael <Mikeks81@gmail.com>
2016-01-02 17:30:24 -0500

    added fuctional navigation to lightbox.

--- script.js
+++ script.js
@@ -10,27 +10,74 @@ $(document).ready(
 
 		// ###### Gallery Page #########
 
-		$(".image").on('click',
+		$(".image").on('click', // shows light box and clicked image
 			function(){
-				$("#lb_image").attr('src',($(this).attr('alt')));
-				$("#outer_lightbox").fadeIn(500);
-				$("#inner_lightbox").fadeIn(500);
-				$("#lb_image").fadeIn(500);
+				var currentImageMarker = $(this).addClass('currentImage');
+
+				//array of img tags in gallery_frames
+				// need to pick out alt attribute for high res file path 
+				var collection = $("#gallery_frames img").siblings(); 
+
+				// what the next image is from thumbnail click
+				// var nextImage = $(".currentImageMarker").next().attr('alt');
+				var nextImage = $(".currentImage").next().attr('alt');
+
+				// what the previous image is from thumbnail click
+				var prevImage = $(".currentImage").prev().attr('alt');
+
+				// code below make the lightbox image = the alt path
+				// in the thumbnail clicked
+				var currentImageVal = currentImageMarker.attr('alt');
+				
+				$("#lb_image").attr('src',($(this).attr('alt'))); 
+				$("#outer_lightbox").fadeIn(400)
+				
+				$("#f_arrow").on('click', //navigation arrows forward
+					function(){
+
+						// if .current class is undefined...
+						if ($('.currentImage').val() === undefined ) { 
+							// make first img tag in #gallery_frames class .currentimage
+							$('#gallery_frames img').first().addClass('currentImage'); 
+							
+							$('#lb_image').attr('src',($('.currentImage').attr('alt')));
+						}
+
+						else {
+						// adds class to the next sibling of the currentImage class
+						$('.currentImage').next().addClass('currentImage'); 
+
+						// removes the class from the previous image 
+						$('.currentImage').first().removeClass('currentImage');
+
+						// tells lb image to use .currentImage alt info as src path
+						$('#lb_image').attr('src',($('.currentImage').attr('alt')));
+						}
+					});
+	
+				$("#b_arrow").on('click', //navigation arrows forward
+					function(){
+
+						// if .current class is undefined...
+						if ($('.currentImage').val() === undefined ) {
+							//make last img tag in #gallery_frames a class of .currentImage
+							$('#gallery_frames img').last().addClass('currentImage');
+							$('#lb_image').attr('src',($('.currentImage').attr('alt')));
+						}
+						else {
+						// adds class to the previous sibling of the currentImage class
+						$('.currentImage').prev().addClass('currentImage'); 
+
+						// removes the class from the previous image 
+						$('.currentImage').last().removeClass('currentImage');
+
+
+						// tells lb image to use .currentImage alt info as src path
+						$('#lb_image').attr('src',($('.currentImage').attr('alt')));
+						}
+					});
 			});
 
-		// $(".image").mouseenter( //on mouseenter of image the image zooms 5px
-		// 	function(){
-		// 		$(this, "img").animate({width: "+=5px",
-		// 			height: "+=5px"});
-		// 	});
-		// $(".image").mouseleave( //on mouseleave of image the image returns to normal
-		// 	function(){
-		// 		$(this, "img").animate({width: "-=5px",
-		// 			height: "-=5px"});
-		// 	});
-
-		// Used CSS to do the animation instead
-		// DO NOT DELETE ABOVE CODE - IMG SELECTOR CODE IS DOPES!!
 
 		//########### Contact Page ###########		
 });		
